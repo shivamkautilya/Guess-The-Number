3540410ea609e0fe4bbd6b353da682ff16d70c9c
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-08 19:03:36 +0530

    Add new features.New features are mentioned below: 1) High Score 2) Lost Game - BG Color changes to red when score goes below. 3) Give msg when Invalid Number is entered. 4) Warn users when number is not between 1 - 20

--- script.js
+++ script.js
@@ -1,64 +1,78 @@
 "use strict";
-/*
-// LECTURE 71 AND 72
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = " Correct Number !!";
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 17;
-document.querySelector(".guess").value = 24;
-
-console.log(
-  document.querySelector(".number").textContent,
-  document.querySelector(".score").textContent
-  // document.querySelector(".guess").value;
-);
-
-console.log(document.querySelector(".guess").value);
-*/
-
-//LECTURE 73: HANDLING CLICK EVENTS
 //functions
-const numberGenerator = function () {
+let numberGenerator = function () {
   return Math.trunc(Math.random() * 20) + 1;
 };
-
+//function for again button
+function clickAgain() {
+  numberGenerator();
+  computerGuess = numberGenerator();
+  userScore = 20;
+  console.log("Correct Answer = ", computerGuess);
+  document.body.style.backgroundColor = "#222";
+  document.querySelector(".message").textContent = "Start Guessing...";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".score").value = 20;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = 0;
+}
+//funtion for game logic
 const clickCheck = function () {
   guessInput = document.querySelector(".guess").value;
   guessInput = parseInt(guessInput);
   console.log("User guessed number = ", guessInput, typeof guessInput);
   // document.querySelector(".message").textContent = "Correct Number";
-  if (guessInput !== computerGuess) {
+  if (guessInput > 20 || guessInput === 0) {
+    document.querySelector(".message").textContent =
+      "Guess number between 1-20";
+    console.log("Guess number between 1-20");
+  } else if (guessInput !== computerGuess) {
     // console.log("if statement executed");
-    userScore = userScore - 1;
-    document.querySelector(".score").textContent = userScore;
-    console.log("User Score = ", userScore);
-    if (guessInput < computerGuess) {
-      document.querySelector(".message").textContent =
-        "Too low!!! Guess higher number.";
-    } else if (guessInput > computerGuess) {
-      document.querySelector(".message").textContent =
-        "Too high!!! Guess lower number.";
-    } else {
-      console.log("Unknown Error!!");
+    if (userScore === 1) {
+      document.body.style.backgroundColor = "#d30f0f";
+      document.querySelector(".message").textContent = "You Lost 🥲";
+      document.querySelector(".score").textContent = 0;
+    } else if (userScore > 1) {
+      userScore--;
+      document.querySelector(".score").textContent = userScore;
+      console.log("User Score = ", userScore);
+      if (guessInput < computerGuess) {
+        document.querySelector(".message").textContent =
+          "Too low!!! Guess higher number.";
+      } else if (guessInput > computerGuess) {
+        document.querySelector(".message").textContent =
+          "Too high!!! Guess lower number.";
+      } else if (!guessInput) {
+        document.querySelector(".message").textContent =
+          "Please enter valid Number!";
+      } else {
+        console.log("Unknown Error!!");
+      }
     }
   } else if (guessInput === computerGuess) {
     document.querySelector(".message").textContent = "🎉 Correct Answer!!!!";
-    document.querySelector(".highscore").textContent = userScore;
+    // document.querySelector(".highscore").textContent = userScore;
     document.querySelector(".number").textContent = guessInput;
     document.body.style.backgroundColor = "#19a447";
     console.log("Correct Guess!!!", "User Score = ", userScore);
+    if (userScore > highScore) {
+      highScore = userScore;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else {
     console.log("Unknown Error");
   }
 };
+
 //
-const computerGuess = numberGenerator();
+let computerGuess = numberGenerator();
 let userScore = document.querySelector(".score").textContent;
 userScore = Number.parseInt(userScore);
 let guessInput = 0;
-// console.log("Correct Answer = ", computerGuess);
+let highScore = 0;
+console.log("Correct Answer = ", computerGuess);
 //Store input of check class
-
 document.querySelector(".check").addEventListener("click", clickCheck);
-//Define Logic to verify user guess is === computerGuess
+//add functionality in again button
+document.querySelector(".again").addEventListener("click", clickAgain);
